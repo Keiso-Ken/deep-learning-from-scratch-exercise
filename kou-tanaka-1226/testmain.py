cff6a61391298f0d36b214f459298254f17fd593
kou-tanaka-1226 <kou-tanaka@cgsys.co.jp>
2020-08-07 11:30:44 +0900

    re-create as another branch : added nand and derivatives.

---
+++ kou-tanaka-1226/testmain.py
@@ -0,0 +1,65 @@
+import numpy as np
+import binarylogicaloperation as blo
+import add as add1
+
+
+print("########################################")
+print("# Get all patterns of binary operation #")
+print("########################################")
+print(
+"P" + "\t" + 
+"Q" + "\t" + 
+"NAND" + "\t" +
+"AND" + "\t" +
+"XP" + "\t" +
+"NXP" + "\t" +
+"XQ" + "\t" +
+"NXQ" + "\t" +
+"ATRUE" + "\t" +
+"AFALSE" + "\t" +
+"ISP" + "\t" +
+"NOTP" + "\t" +
+"ISQ" + "\t" +
+"NOTQ" + "\t" +
+"OR" + "\t" +
+"NOR" + "\t" +
+"XOR" + "\t" +
+"NXOR"
+)
+for p in [0,1]:
+	for q in [0,1]:
+		print(p, end="\t")
+		print(q, end="\t")
+		print(blo.nand_(p, q), end="\t")
+		print(blo.and_(p, q),  end="\t")
+		print(blo.xp_(p, q), end="\t")
+		print(blo.nxp_(p, q), end="\t")
+		print(blo.xq_(p, q), end="\t")
+		print(blo.nxq_(p, q), end="\t")
+		print(blo.atrue_(p, q), end="\t")
+		print(blo.afalse_(p, q), end="\t")
+		print(blo.isp_(p, q), end="\t")
+		print(blo.notp_(p, q), end="\t")
+		print(blo.isq_(p, q), end="\t")
+		print(blo.notq_(p, q), end="\t")
+		print(blo.or_(p, q), end="\t")
+		print(blo.nor_(p, q), end="\t")
+		print(blo.xor_(p, q), end="\t")
+		print(blo.nxor_(p, q))
+
+print("")
+print("########################################")
+print("## Calculate 5 vs 5 binary digits sum ##")
+print("########################################")
+
+a=[0,0,1,0,1]
+b=[0,0,1,1,1]
+
+c = add1.add55(a,b)
+a_ = [0] + a
+b_ = [0] + b
+print(a_)
+print("+")
+print(b_)
+print("=")
+print(c)
