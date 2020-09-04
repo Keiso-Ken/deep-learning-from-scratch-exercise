61f197a74417def06d62e5f315490555b9576899
Kouta Tanaka <kou-tanaka@cgsys.co.jp>
2020-09-04 10:12:11 +0900

    Deleted old folder.

--- chapter03/text/3_2_2_step_function_impl_01.py
+++
@@ -1,9 +0,0 @@
-def step_function(x):
-    if x > 0:
-        return 1
-    else:
-        return 0
-
-# test
-x = 0.1
-print(step_function(x))
--- chapter03/text/3_2_7_ReLU_function.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def relu(x):
-    return np.maximum(0,x)
-
-# test
-x = np.array([-1.0, 1.0, 2.0])
-print(relu(x))
